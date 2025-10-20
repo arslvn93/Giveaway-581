57aeb3c289349777b546af438a80c3539e67f542
arslvn93 <support@forms.ltd>
2025-10-20 14:35:36 -0400

    Add Cloudflare Turnstile bot protection and honeypot field

--- config.js
+++ config.js
@@ -31,7 +31,8 @@ const config = {
     "facebookPixelId": "1944426839435015",
     "salesGeniusAppApi": "",
     "followUpBossEmail": "",
-    "webhookUrl": "https://n8n.salesgenius.co/webhook/giveawayupdate"
+    "webhookUrl": "https://n8n.salesgenius.co/webhook/giveawayupdate",
+    "turnstileSiteKey": "0x4AAAAAAB5SpbWeBo7JnEIs"
   },
   "prize": {
     "images": [
