8223ea6146b063184b13ae805f96073b080fc9dd
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-10-22 16:23:31 -0400

    Update Site

--- config.js
+++ config.js
@@ -176,7 +176,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1761164276171_0",
+      "id": "q_auto_1761164610471_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -198,7 +198,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1761164276171_1",
+      "id": "q_auto_1761164610472_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -220,7 +220,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1761164276171_2",
+      "id": "q_auto_1761164610472_2",
       "questionText": "What’s your current housing situation?",
       "options": [
         {
@@ -261,7 +261,7 @@ const config = {
     },
     "agentContact": {
       "name": "Nicole Masters",
-      "title": "Realtor",
+      "title": "REALTOR® | DRE #02147150",
       "phone": "916-836-1449",
       "email": "nicole@mastersrealty.homes"
     },
